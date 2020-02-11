0c1bdcdbe9d338c571ff85dc4dc8c0551625668d
Karey Higuera <karey.higuera@gmail.com>
2020-02-11 14:29:52 -0600

    add initial podcast list

---
+++ src/data/podcasts.js
@@ -0,0 +1,17 @@
+import FindingDrago from '../assets/podcasts/findingdrago.jpg'
+import Syntax from '../assets/podcasts/syntax.png'
+
+export default [
+  {
+    title: "Finding Drago",
+    link: "https://www.abc.net.au/radio/programs/finding-drago/",
+    description: "",
+    logo: {FindingDrago}
+  },
+  {
+    title: "SyntaxFM",
+    link: "https://syntax.fm/",
+    description: "",
+    logo: {Syntax}
+  }
+]
