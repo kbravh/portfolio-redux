78b4d89e4b0b5f91f300133a7a05fdc58716ad22
Karey Higuera <karey.higuera@gmail.com>
2020-02-10 20:56:48 -0600

    add code blocks and syntax highlighting

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,45 @@
 module.exports = {
-  plugins: [
-    {
+  plugins: [{
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
         path: `${__dirname}/src/`,
       },
     },
-    `gatsby-transformer-remark`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [{
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 590,
+              withWebp: true
+            },
+          },
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              // This is used to allow setting a language for inline code
+              // (i.e. single backticks) by creating a separator.
+              // This separator is a string and will do no white-space
+              // stripping.
+              inlineCodeMarker: `±`,
+              // Customize the prompt used in shell output
+              // Values below are default
+              prompt: {
+                user: "kbravh",
+                host: "localhost",
+                global: true,
+              }
+            },
+          },
+        ],
+      },
+    },
     // {
     //   resolve: `gatsby-plugin-layout`,
     //   options: {
