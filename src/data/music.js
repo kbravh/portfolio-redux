aa0bb445d63c0832abc831931aa0062b0f213ca7
Karey Higuera <karey.higuera@gmail.com>
2020-02-11 21:04:30 -0600

    add music folder and file

---
+++ src/data/music.js
@@ -0,0 +1,9 @@
+import MadeInHeights from '../assets/music/madeinheights.jpeg'
+
+export default [
+  {
+    title: "Made in Heights",
+    link: "http://madeinheights.com/",
+    image: MadeInHeights
+  }
+]
