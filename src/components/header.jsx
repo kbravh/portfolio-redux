706ca2ed2931b0e1810d97a74d8fe3f99515cb81
Karey Higuera <karey.higuera@gmail.com>
2020-02-13 15:14:13 -0600

    add breakpoints for layout padding

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,12 +3,15 @@ import { Link } from 'gatsby'
 
 import '../css/header.css'
 
+import Resume from '../assets/Karey_Higuera_Resume.pdf'
+
 const Header = () => (
   <nav>
-    <div className="logo">Kbravh</div>
     <div className="navlinks">
       <Link to="/">Home</Link>
       <Link to="/blog">Blog</Link>
+      <Link to="/about">About</Link>
+      <a className="resume-link" download href={Resume}>Resume</a>
     </div>
   </nav>
 )
