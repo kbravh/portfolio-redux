e3df8a518544c613279a1fa7f710634e535ea32b
Karey Higuera <karey.higuera@gmail.com>
2020-02-14 13:44:46 -0600

    add intro banner

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,12 @@
-import React from "react"
+import React, {useEffect} from "react"
 import Layout from '../components/layout'
 
-export default () => <Layout>Hello world!</Layout>
+export default () => {
+return (
+    <Layout>
+      <h1>Karey Higuera</h1>
+      <h2>Full-stack developer</h2>
+      <p>I'm a full stack developer passionate about modern web technologies. I work with JavaScript and React on the front-end and Node.js in cloud services on the backend. I enjoy utilizing serverless functionality to make the web a simpler, more maintainable space.</p>
+    </Layout>
+  )
+}
