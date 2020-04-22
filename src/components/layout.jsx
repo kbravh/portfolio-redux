20e6f89502b2f82f5a141a94d4ed6a4dd97fd245
Karey Higuera <karey.higuera@gmail.com>
2020-04-22 08:05:28 -0500

    join paths on codepen logo

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -10,9 +10,9 @@ const Layout = ({ children, pageContext }) => {
   }
   return (
     <>
-    <Helmet>
-      
-    </Helmet>
+      <Helmet>
+
+      </Helmet>
       <Header />
       <div className="layout-wrapper">
         <div className="left-sidebar">
@@ -38,12 +38,9 @@ const Layout = ({ children, pageContext }) => {
             </li>
             <li>
               <a href="https://codepen.io/kbravh" alt="CodePen">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="feather feather-codepen" viewBox="0 0 24 24">
-                  <title>CodePen</title>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" class="feather feather-codepen" viewBox="0 0 24 24">
                   <defs />
-                  <path d="M12 2l10 6.5v7L12 22 2 15.5v-7L12 2zM12 22v-6.5" />
-                  <path d="M22 8.5l-10 7-10-7" />
-                  <path d="M2 15.5l10-7 10 7M12 2v6.5" />
+                  <path d="M2 15.5l10-7 10 7M12 2v6.5m10 0l-10 7-10-7M12 2l10 6.5v7L12 22 2 15.5v-7zm0 20v-6.5" />
                 </svg>
               </a>
             </li>
