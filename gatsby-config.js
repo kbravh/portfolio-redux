545a8f5a074e18b93a0d51cc8d66c9ce71866cf6
Karey Higuera <karey.higuera@gmail.com>
2020-06-11 22:55:16 -0500

    add web monetization to site

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,103 +6,109 @@ module.exports = {
     siteUrl: `https://kbravh.dev`
   },
   plugins: [{
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `src`,
-        path: `${__dirname}/src/`,
-      },
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `src`,
+      path: `${__dirname}/src/`,
     },
+  },
+  {
+    resolve: `gatsby-plugin-monetization`,
+    options: {
+      paymentPointer: '$ilp.uphold.com/BfyYUyGBrEgA',
+    },
+  },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
-    {
-      resolve: `gatsby-plugin-mdx`,
-      options: {
-        extensions: [`.mdx`, `.md`],
-        gatsbyRemarkPlugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 590,
-              withWebp: true
-            },
+  {
+    resolve: `gatsby-plugin-mdx`,
+    options: {
+      extensions: [`.mdx`, `.md`],
+      gatsbyRemarkPlugins: [
+        {
+          resolve: `gatsby-remark-images`,
+          options: {
+            // It's important to specify the maxWidth (in pixels) of
+            // the content container as this plugin uses this as the
+            // base for generating different widths of each image.
+            maxWidth: 590,
+            withWebp: true
           },
-          {
-            resolve: `gatsby-remark-autolink-headers`,
-            options: {
-              maintainCase: true,
-              removeAccents: true,
-              isIconAfterHeader: true,
-            },
-          },
-          `gatsby-remark-external-links`,
-          {
-            resolve: `gatsby-remark-prismjs`,
-            options: {
-              // This is used to allow setting a language for inline code
-              // (i.e. single backticks) by creating a separator.
-              // This separator is a string and will do no white-space
-              // stripping.
-              inlineCodeMarker: `~`,
-              // Customize the prompt used in shell output
-              // Values below are default
-              prompt: {
-                user: "kbravh",
-                host: "home",
-                global: true,
-              }
-            },
-          },
-        ],
-      },
-    },
-    {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
-      options: {
-        fonts: [{
-            family: `Odibee Sans`,
-          },
-          {
-            family: `Unica One`,
-            // text: ` JAaNnuryFebMOchpilgStcvD1234567890,`
-          },
-          {
-            family: `Andika`,
-          }
-        ],
-      },
-    },
-    {
-      resolve: "gatsby-plugin-web-font-loader",
-      options: {
-        custom: {
-          families: [""],
-          urls: ["/fonts/fonts.css"],
         },
+        {
+          resolve: `gatsby-remark-autolink-headers`,
+          options: {
+            maintainCase: true,
+            removeAccents: true,
+            isIconAfterHeader: true,
+          },
+        },
+        `gatsby-remark-external-links`,
+        {
+          resolve: `gatsby-remark-prismjs`,
+          options: {
+            // This is used to allow setting a language for inline code
+            // (i.e. single backticks) by creating a separator.
+            // This separator is a string and will do no white-space
+            // stripping.
+            inlineCodeMarker: `~`,
+            // Customize the prompt used in shell output
+            // Values below are default
+            prompt: {
+              user: "kbravh",
+              host: "home",
+              global: true,
+            }
+          },
+        },
+      ],
+    },
+  },
+  {
+    resolve: `gatsby-plugin-prefetch-google-fonts`,
+    options: {
+      fonts: [{
+        family: `Odibee Sans`,
+      },
+      {
+        family: `Unica One`,
+        // text: ` JAaNnuryFebMOchpilgStcvD1234567890,`
+      },
+      {
+        family: `Andika`,
+      }
+      ],
+    },
+  },
+  {
+    resolve: "gatsby-plugin-web-font-loader",
+    options: {
+      custom: {
+        families: [""],
+        urls: ["/fonts/fonts.css"],
       },
     },
-    {
-      resolve: `gatsby-plugin-layout`,
-      options: {
-        component: require.resolve(`./src/components/layout.jsx`),
-      },
+  },
+  {
+    resolve: `gatsby-plugin-layout`,
+    options: {
+      component: require.resolve(`./src/components/layout.jsx`),
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Karey Higuera Portfolio`,
-        short_name: `kbravh`,
-        start_url: `/`,
-        background_color: `#f7f7f7`,
-        theme_color: `#102542`,
-        display: `standalone`,
-        icon: `src/assets/favicon.png`
-      },
+  },
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `Karey Higuera Portfolio`,
+      short_name: `kbravh`,
+      start_url: `/`,
+      background_color: `#f7f7f7`,
+      theme_color: `#102542`,
+      display: `standalone`,
+      icon: `src/assets/favicon.png`
     },
+  },
     `gatsby-plugin-offline`,
   ]
 }
