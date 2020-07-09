e5f3c9fa8c813b58a159550b55ed096ea563c172
Karey Higuera <karey.higuera@gmail.com>
2020-07-08 21:48:29 -0500

    Add project link to header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -9,6 +9,7 @@ const Header = () => (
   <nav>
     <div className="navlinks">
       <Link to="/">Home</Link>
+      <Link to='/projects'>Projects</Link>
       <Link to="/writing">Writing</Link>
       <span>
         <a className="resume-link" download href={Resume}>Resume</a>
