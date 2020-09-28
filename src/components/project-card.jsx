3963408ee9a95fb174dc7af3585f55647f655862
Karey Higuera <karey.higuera@gmail.com>
2020-09-27 20:55:41 -0500

    add logos to project cards

--- src/components/project-card.jsx
+++ src/components/project-card.jsx
@@ -5,12 +5,17 @@ import '../css/project-card.css'
 const ProjectCard = ({ project }) => {
     return (
         <div className="project-card">
-            <h2>{project.frontmatter.title}</h2>
-            <div>{project.frontmatter.description}</div>
-            <div className="project-tags">
-                {project.frontmatter.tags.map(tag => (
-                    <span className="project-tag" key={tag}>{tag}</span>
-                ))}
+            <div class="project-logo">
+                <img src={`logos/${project.frontmatter.slug}.png`} alt={`${project.frontmatter.title} logo`}/>
+            </div>
+            <div className="project-info">
+                <h2>{project.frontmatter.title}</h2>
+                <div>{project.frontmatter.description}</div>
+                <div className="project-tags">
+                    {project.frontmatter.tags.map(tag => (
+                        <span className="project-tag" key={tag}>{tag}</span>
+                    ))}
+                </div>
             </div>
         </div>
     )
