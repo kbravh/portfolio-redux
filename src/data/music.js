d0e56aa83b737ef9a450afcf4b8484936f678214
Karey Higuera <karey.higuera@gmail.com>
2021-05-05 20:22:56 -0500

    remove unused assets

--- src/data/music.js
+++
@@ -1,9 +0,0 @@
-import MadeInHeights from '../assets/music/madeinheights.jpeg'
-
-export default [
-  {
-    title: "Made in Heights",
-    link: "http://madeinheights.com/",
-    image: MadeInHeights
-  }
-]
