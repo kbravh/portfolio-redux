c4ef988a1a980195be9d2b8cefa414fda8424aaf
Karey Higuera <karey.higuera@gmail.com>
2022-01-10 19:21:43 -0600

    update reading list

--- src/data/reading_list.js
+++ src/data/reading_list.js
@@ -1,5 +1,19 @@
 export const readingListRaw = [
   {
+    title: 'Journey to the Center of the Earth',
+    author: 'Jules Verne',
+    comment: "I've had this classic on my shelf for a while and I'm excited to knock it out!",
+    cover: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1389754903l/32829.jpg',
+    url: 'https://www.goodreads.com/book/show/32829.Journey_to_the_Center_of_the_Earth'
+  },
+  {
+    title: 'The Address Book',
+    author: ' Deirdre Mask',
+    comment: 'A dive into the importance and history of personal home addresses. I saw this mentioned on the PostCrossing blog.',
+    cover: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1567541521l/45046690.jpg',
+    url: 'https://www.goodreads.com/book/show/45046690-the-address-book'
+  },
+  {
     title: 'La Desaparición del Principito',
     author: 'Jaoquín Guerrero Casasola',
     comment: 'A light fiction novel about the disappearance of Antoine Saint-Exupéry, the author of Le Petit Prince. I found it in a bookstore in Mexico and figured I\'d give it a shot.',
@@ -26,13 +40,6 @@ export const readingListRaw = [
     comment: 'I knocked the series out in 2020, and I *still* think about the story about once a week. I want to give it another read-through and enjoy it again.',
     cover: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1415428227l/20518872.jpg',
     url: 'https://www.goodreads.com/book/show/20518872-the-three-body-problem'
-  },
-  {
-    title: 'Journey to the Center of the Earth',
-    author: 'Jules Verne',
-    comment: "I've had this classic on my shelf for a while and I'm excited to knock it out!",
-    cover: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1389754903l/32829.jpg',
-    url: 'https://www.goodreads.com/book/show/32829.Journey_to_the_Center_of_the_Earth'
   },
   // {
   //   title: '',
